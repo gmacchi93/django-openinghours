70b0bddeb933e01a4ea8c966dbbe6b3440195f4e
Guillermo Macchi <guillemacchi01@gmail.com>
2016-07-08 10:58:44 -0400

    Update urls.py

--- openinghours/urls.py
+++ openinghours/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from openinghours.views import CurrentlyOpenView
 from openinghours.views_edit import OpeningHoursEditView
+from django.contrib.auth.decorators import permission_required
+
 
 urlpatterns = [
      url(r'^$', CurrentlyOpenView.as_view(),
          name='openinghours_currently_open'),
-     url(r'^edit/(?P<pk>[0-9]+)$', OpeningHoursEditView.as_view(),
+     url(r'^edit/(?P<pk>[0-9]+)$', permission_required('is_staff')(OpeningHoursEditView.as_view()),
          name='openinghours_edit'),
 ]
+
+
